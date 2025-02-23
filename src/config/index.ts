ad13e403eafd302a79df5fda1421739167e80a69
Bongani Masuku <bongani@1702tech.com>
2025-02-23 11:53:37 +0200

    OPH-101 - logging port type

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,10 +9,12 @@ export type Config = {
 }
 
 export const _config = (): Config => {
-  console.log("process.env.COMINGSOON_PORT", process.env.COMINGSOON_PORT)
+  const port = process.env.COMINGSOON_PORT || "3000"
+  console.log("port : ", port)
+  console.log("typeof port : ", typeof port)
   return {
     protocol: process.env.COMINGSOON_PROTOCOL || "http",
     host: process.env.COMINGSOON_HOST || "localhost",
-    port: parseInt(process.env.COMINGSOON_PORT || "3000", 10),
+    port: parseInt(port, 10),
   }
 }
