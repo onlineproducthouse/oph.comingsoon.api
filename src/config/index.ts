1c1369fce7c588e819d5f6304bf0c0c640867c1c
Bongani Masuku <bongani@1702tech.com>
2025-02-23 11:42:17 +0200

    OPH-101 - log port

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,8 +8,11 @@ export type Config = {
   port: number
 }
 
-export const _config = (): Config => ({
-  protocol: process.env.COMINGSOON_PROTOCOL || "http",
-  host: process.env.COMINGSOON_HOST || "localhost",
-  port: parseInt(process.env.COMINGSOON_PORT || "3000", 10),
-})
+export const _config = (): Config => {
+  console.log("process.env.COMINGSOON_PORT", process.env.COMINGSOON_PORT)
+  return {
+    protocol: process.env.COMINGSOON_PROTOCOL || "http",
+    host: process.env.COMINGSOON_HOST || "localhost",
+    port: parseInt(process.env.COMINGSOON_PORT || "3000", 10),
+  }
+}
