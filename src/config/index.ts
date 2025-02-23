c27a913c6cfb4904be24740fc6d8613fab0a77cd
Bongani Masuku <bongani@1702tech.com>
2025-02-23 12:12:56 +0200

    OPH-101 - remove logs

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,13 +8,8 @@ export type Config = {
   port: number
 }
 
-export const _config = (): Config => {
-  const port = process.env.COMINGSOON_PORT || "3000"
-  console.log("port : ", port)
-  console.log("typeof port : ", typeof port)
-  return {
-    protocol: process.env.COMINGSOON_PROTOCOL || "http",
-    host: process.env.COMINGSOON_HOST || "localhost",
-    port: parseInt(port, 10),
-  }
-}
+export const _config = (): Config => ({
+  protocol: process.env.COMINGSOON_PROTOCOL || "http",
+  host: process.env.COMINGSOON_HOST || "localhost",
+  port: parseInt(process.env.COMINGSOON_PORT || "3000", 10),
+})
