7aad8a69b22a1932fcf02d4c7e2c4b94f63949b2
Bongani Masuku <bongani@1702tech.com>
2025-02-24 19:04:17 +0200

    OPH-101 - add environment variable: COMINGSOON_FOR_PROJECT

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,10 +6,12 @@ export type Config = {
   protocol: string
   host: string
   port: number
+  forProject: string
 }
 
 export const _config = (): Config => ({
   protocol: process.env.COMINGSOON_PROTOCOL || "http",
   host: process.env.COMINGSOON_HOST || "localhost",
   port: parseInt(process.env.COMINGSOON_PORT || "3000", 10),
+  forProject: process.env.COMINGSOON_FOR_PROJECT || "unknown",
 })
